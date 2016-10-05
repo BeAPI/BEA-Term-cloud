4166f9f2eed824638b52c98cf62fd79a9ed12bb3
Nicolas Kulka <kulka.nicolas@gmail.com>
2016-10-05 16:20:29 +0200

    Update credentials

--- bea-term-cloud.php
+++ bea-term-cloud.php
@@ -2,27 +2,28 @@
 /*
  Plugin Name: BEA - Term Cloud
  Version: 1.0.0
- Plugin URI: http://beapi.fr
+ Plugin URI: http://www.beapi.fr
  Description: Allow in visual composer and widgets (to be done) to display a beautiful term cloud.
  Tags: tag, tags, terms, term, taxonomy, taxonomies, visual composer
- Author: BeAPI
+ Author: BE API Technical team
  Author URI: http://www.beapi.fr
  Domain Path: languages
  Text Domain: bea-term-cloud
+ 
  ----
-
- Copyright 2016 Beapi (technique@beapi.fr)
-
+ 
+ Copyright 2016 BE API Technical team (human@beapi.fr)
+ 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
-
+ 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
-
+ 
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
